090f75b9579fe974d0b4f27060c6821d036b6e64
achmad21 <achmadhasrulwinada@gmail.com>
2020-01-10 22:07:33 +0700

    first commit

---
+++ src/main/java/com/bandingintest/ahmad/AhmadApplication.java
@@ -0,0 +1,13 @@
+package com.bandingintest.ahmad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhmadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandingintest/ahmad/AhmadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandingintest.ahmad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
